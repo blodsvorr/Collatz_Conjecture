3781af73a5385087acfe737b28329e2810c3e5de
Alex <46936986+blodsvorr@users.noreply.github.com>
2023-01-25 18:38:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,72 @@
-class CollatzExecute {
+class CollatzFunction {
+constructor(start = 2) {
+this.start = start;
+this.currentStep = start;
+this.stepCounter = 1;
+this.horizLength = 0;
+this.hLengthHold = 0;
+}
+
+// GETTERS AND SETTERS
+setStart(start) {
+this.start = start;
+this.currentStep = start;
+this.stepCounter = 1;
+this.horizLength = 0;
+}
+
+getStart() {
+return this.start;
+}
+
+getCurrentStep() {
+return this.currentStep;
+}
+
+setHorizLength(hLength) {
+this.horizLength = hLength;
+}
+
+getHorizLength() {
+return this.horizLength;
+}
+
+getHLengthHold() {
+return this.hLengthHold;
+}
+
+setHLengthHold(hLengthHold) {
+this.hLengthHold = hLengthHold;
+}
+
+getStepCounter() {
+return this.stepCounter;
+}
+
+// OTHER METHODS
+nDivTwo() {
+let quotient = this.currentStep / 2;
+return quotient;
+}
+
+threeNLessOne() {
+let prodSum = 3 * this.currentStep + 1;
+return prodSum;
+}
+
+takeStep() {
+let nextStep = this.currentStep % 2 == 0 ? this.nDivTwo() : this.threeNLessOne();
+this.currentStep = nextStep;
+this.stepCounter += 1;
+}
+
+addToHLength(hAdd) {
+this.horizLength += hAdd;
+}
+
+}
+
+function CollatzExecute () {
         let start = 43;
 
         let cc1 = new CollatzFunction(start);
