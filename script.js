083fa47f4d7cefb8d0142b9c71b157d5a35561d7
Alex <46936986+blodsvorr@users.noreply.github.com>
2023-02-20 13:32:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -64,63 +64,73 @@ class CollatzFunction {
   }
 }
 
-function CollatzExecute () {
-  let start = 293;
-  let cc1 = new CollatzFunction(start);
-  let thisStep = cc1.getCurrentStep();
-  let lastStep = thisStep;
-  let str = thisStep;
+function CollatzExecute ( s_in ) {
+        let start = s_in;
+        if ( start <= 0 ) {
+          start = 4 ;
+        }
+        let cc1 = new CollatzFunction(start);
+        let thisStep = cc1.getCurrentStep();
+        let lastStep = thisStep;
+        let str = thisStep;
 
-  while (thisStep !== 1) {
-    let thisLEN = String(thisStep).length;
+        while (thisStep !== 1) {
+          let thisLEN = String(thisStep).length;
           
-      if (thisStep % 2 === 0) {
-        let dent = "";
-        let ws = "" ;
-        
-        for (let i = thisLEN - 1; i > 0; i--) {
-          dent += "&nbsp;";
-        }
-        for (let i = cc1.getHorizLength(); i > 0; i--) {
-          ws += "&nbsp;";
-        }
-           
-        str +="<br>" + ws;
-        str += dent + "|";
-        str += "<br>" + ws;
-      }
-      else {
-        str += "——";
-        cc1.addToHLength(2);
-        cc1.addToHLength(thisLEN);
-      }
-
-      lastStep = thisStep;
-      cc1.takeStep();
-      thisStep = cc1.getCurrentStep();
-      
-      if (lastStep % 2 === 0) {
-        thisLEN = String(thisStep).length;
-        let lastLEN = String(lastStep).length;
-        let diff = lastLEN - thisLEN;
-        for (let i = diff; i > 0; i--) {
-          str += "&nbsp;";
-          cc1.addToHLength(1);
-        }
-      }
+            if (thisStep % 2 === 0) {
+              let dent = "";
+              let ws = "" ;
+                
+              for (let i = thisLEN - 1; i > 0; i--) {
+                dent += "&nbsp;";
+              }
+              for (let i = cc1.getHorizLength(); i > 0; i--) {
+                ws += "&nbsp;";
+              }
+                
+              str +="<br>" + ws;
+              str += dent + "|";
+              str += "<br>" + ws;
+                
+            } else {
+                str += " —— ";
+                cc1.addToHLength(4);
+                cc1.addToHLength(thisLEN);
+            }
+            lastStep = thisStep;
+            cc1.takeStep();
+            thisStep = cc1.getCurrentStep();
             
-      str += thisStep;
-  }
+            if (lastStep % 2 === 0) {
+              thisLEN = String(thisStep).length;
+              let lastLEN = String(lastStep).length;
+              let diff = lastLEN - thisLEN;
+              for (let i = diff; i > 0; i--) {
+                str += "&nbsp;";
+                cc1.addToHLength(1);
+              }
+            }
+            
+            str += thisStep;
+        }
         
-  ctr = "Number of Steps";
-  ctr += "<br>";
-  ctr += cc1.getStepCounter();
-  ctr += "<br><br>";
+        ctr = "Number of Steps";
+        ctr += "<br>";
+        ctr += cc1.getStepCounter();
+        ctr += "<br><br>";
         
-  str = ctr + str ;
+        str = ctr + str ;
 
-  document.getElementById ( 'CC' ).style.width = cc1.getHorizLength ;
-  document.getElementById ( 'CC' ).innerHTML = str ;
+        document.getElementById ( 'CC' ).style.width = cc1.getHorizLength ;
+        document.getElementById ( 'CC' ).innerHTML = str ;
 }
 
-CollatzExecute() ;
+let input = "" ;
+function start_in () {
+  input = inEl.value
+}
+let inEl = document.getElementById ( 'in' ) ;
+let bttnEl = document.getElementById ( 'button' ) ;
+
+inEl.oninput = function() {start_in()} ;
+bttnEl.onclick = function() {CollatzExecute(input)};
